41c943245ac4c5687d47d5ec314b05a5fad5805f
XxKrafterxX <43410077+XxKrafterxX@users.noreply.github.com>
2020-04-09 11:49:25 -0400

    Update script.py

--- script.py
+++ script.py
@@ -21,6 +21,12 @@ subjectVisit7 = tk.Label(root, text="Visit 7")
 subjectVisit8 = tk.Label(root, text="Visit 8")
 subjectVisit9 = tk.Label(root, text="Visit 9")
 subjectVisit10 = tk.Label(root, text="Visit 10")
+subjectNumberHeading = tk.Label(root, text="Subject #")
+
+
+# subject number boxes
+sbox1 = tk.Entry(root, width="5")
+
 # SUBJECT ONE visit boxes
 s1v1 = tk.Entry(root, width="5")
 s1v2 = tk.Entry(root, width="5")
@@ -44,6 +50,7 @@ s2v8 = tk.Entry(root, width="5")
 s2v9 = tk.Entry(root, width="5")
 s2v10 = tk.Entry(root, width="5")
 
+
 # display the gui
 canvas.grid()
 subjectFirstVisitDate.place(x=0, y=0)
@@ -56,6 +63,10 @@ subjectVisit7.place(x=260, y=0)
 subjectVisit8.place(x=300, y=0)
 subjectVisit9.place(x=340, y=0)
 subjectVisit10.place(x=380, y=0)
+subjectNumberHeading.place(x=420, y=0)
+# display subject number boxes
+sbox1.place(x=420,y=20)
+
 # display SUBJECT ONE entry boxes
 s1v1.place(x=10, y=20)
 s1v2.place(x=60, y=20)
@@ -83,3 +94,4 @@ s2v10.place(x=380, y=43)
 
 # loop
 root.mainloop()
+
